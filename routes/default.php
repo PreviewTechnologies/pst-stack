25a2b6b372e6ba9ebce66c59bffd6892da9e3a9a
Shaharia Azam <shaharia.azam@gmail.com>
2015-01-31 01:32:36 +0600

    Signup routes, template created

--- routes/default.php
+++ routes/default.php
@@ -37,6 +37,40 @@ $app->post(
         }
     }
 );
+$app->get('/signup', function () use ($app) {
+    if (App::userLoggedIn()) {
+        $app->flash('error', 'You are already logged in');
+        $app->redirect('/');
+    }
+
+    $app->log->info("Slim-Skeleton '/' route");
+
+    $app->render('signup.twig');
+});
+
+$app->post(
+    '/signup',
+    function () use ($app) {
+        $emailAddress = strtolower(filter_var($app->request()->post('email'), FILTER_SANITIZE_STRING));
+        $password = filter_var($app->request()->post('password'), FILTER_SANITIZE_STRING);
+        $firstName = filter_var($app->request()->post('firstName'), FILTER_SANITIZE_STRING);
+        $lastName = filter_var($app->request()->post('lastName'), FILTER_SANITIZE_STRING);
+
+        $user = new User();
+        $user->setFirstName($firstName);
+        $user->setLastName($lastName);
+        $user->setEmailAddress($emailAddress);
+        $user->setPassword(password_hash($password, PASSWORD_BCRYPT));
+        $user->setUUID(UUID::v4());
+        if($user->save()){
+            $app->flash('info', 'Signup successful. You may login now!');
+            $app->redirect('/login');
+        }else{
+            $app->flash('error', 'Something went wrong!');
+            $app->redirect('/signup');
+        }
+    }
+);
 
 $app->get(
     '/logout',
