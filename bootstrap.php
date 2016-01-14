ebaf9f39bb382572db2d61900ceacc55bcac1d44
Sohel Rana <me.sohelrana@gmail.com>
2016-01-14 19:48:48 +0600

    Application has been configured based on SLIM3

--- bootstrap.php
+++ bootstrap.php
@@ -1,35 +1,29 @@
 <?php
+
 define('APP_DIR', __DIR__.DIRECTORY_SEPARATOR);
 
+require 'vendor/autoload.php';
+
+define('APP_DIR', __DIR__.DIRECTORY_SEPARATOR);
 mb_internal_encoding('UTF-8');
 mb_http_output('UTF-8');
 date_default_timezone_set('Asia/Dhaka');
 session_start();
 
-require APP_DIR.'vendor/autoload.php';
+$app = new \Slim\App();
 
-require_once APP_DIR.'propel/generated-conf/config.php';
+// Get container
+$container = $app->getContainer();
 
-// Prepare app
-$app = new \Slim\Slim(array(
-    'templates.path' => APP_DIR.'templates',
-));
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(APP_DIR.'templates', [
+        'cache' => APP_DIR.'tmp/cache'
+    ]);
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $container['router'],
+        $container['request']->getUri()
+    ));
 
-// Create monolog logger and store logger in container as singleton
-// (Singleton resources retrieve the same log resource definition each time)
-$app->container->singleton('log', function () {
-    $log = new \Monolog\Logger('slim-skeleton');
-    $log->pushHandler(new \Monolog\Handler\StreamHandler(APP_DIR.'tmp/app.log', \Monolog\Logger::DEBUG));
-    return $log;
-});
-
-// Prepare view
-$app->view(new \Slim\Views\Twig());
-$app->view->parserOptions = array(
-    'charset' => 'utf-8',
-    'cache' => realpath(APP_DIR.'tmp/cache'),
-    'auto_reload' => true,
-    'strict_variables' => false,
-    'autoescape' => true
-);
-$app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
+    return $view;
+};
