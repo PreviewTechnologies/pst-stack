6d2953a47ea243db4487b019564533d3ec1a2e4a
Shaharia Azam <shaharia.azam@gmail.com>
2016-02-14 02:42:37 +0600

    Utility var_dump removed

--- public/index.php
+++ public/index.php
@@ -2,9 +2,6 @@
 //Application Bootstrap file
 require dirname(__DIR__).DIRECTORY_SEPARATOR.'config/bootstrap.php';
 
-$pst = new \PstStack\Utility\Utility();
-var_dump($pst->test()); die();
-
 //All routes
 require "../routes/default.php";
 
